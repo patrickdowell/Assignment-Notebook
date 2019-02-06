0edd0d85681eb3efd674cabbc4a5974baf89914c
Patrick Dowell <patrickdowell@patrick-ds-macbook-air.local>
2019-02-06 11:16:06 -0600

    Setup storyboard

---
+++ Assignment Notebook/Assignment.swift
@@ -0,0 +1,13 @@
+//
+//  Assignment.swift
+//  Assignment Notebook
+//
+//  Created by Patrick Dowell on 2/6/19.
+//  Copyright © 2019 Patrick Dowell. All rights reserved.
+//
+
+import UIKit
+
+class Assignment: Codable {
+
+}
