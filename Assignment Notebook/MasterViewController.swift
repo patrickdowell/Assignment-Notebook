db74cac643d73b629c88a1aaf5b9722326dd5a87
Patrick Dowell <patrickdowell@patrick-ds-macbook-air.local>
2019-02-08 12:15:41 -0600

    Added data persistence (final)

--- Assignment Notebook/MasterViewController.swift
+++ Assignment Notebook/MasterViewController.swift
@@ -12,6 +12,7 @@ class MasterViewController: UITableViewController {
 
     var detailViewController: DetailViewController? = nil
     var assignments = [Assignment]()
+    let defaults = UserDefaults.standard
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -24,12 +25,19 @@ class MasterViewController: UITableViewController {
             let controllers = split.viewControllers
             detailViewController = (controllers[controllers.count-1] as! UINavigationController).topViewController as? DetailViewController
         }
+        
+        if let saveData = defaults.object(forKey: "data") as? Data {
+            if let decoded = try? JSONDecoder().decode([Assignment].self, from: saveData) {
+                assignments = decoded
+            }
+        }
     }
 
     override func viewWillAppear(_ animated: Bool) {
         clearsSelectionOnViewWillAppear = splitViewController!.isCollapsed
         super.viewWillAppear(animated)
         tableView.reloadData()
+        saveData()
     }
 
     @objc
@@ -49,6 +57,7 @@ class MasterViewController: UITableViewController {
             let assignments = Assignment(title: titleTextField.text!, subject: subjectTextField.text!, dueDate: dueDateTextField.text!, description: descriptionTextView.text!)
             self.assignments.append(assignments)
             self.tableView.reloadData()
+            self.saveData()
         }
         alert.addAction(insertAction)
         present(alert, animated: true, completion: nil)
@@ -95,6 +104,7 @@ class MasterViewController: UITableViewController {
         if editingStyle == .delete {
             assignments.remove(at: indexPath.row)
             tableView.deleteRows(at: [indexPath], with: .fade)
+            saveData()
         } else if editingStyle == .insert {
             // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view.
         }
@@ -103,6 +113,13 @@ class MasterViewController: UITableViewController {
     override func tableView(_ tableView: UITableView, moveRowAt sourceIndexPath: IndexPath, to destinationIndexPath: IndexPath) {
         let objectToMove = assignments.remove(at: sourceIndexPath.row)
         assignments.insert(objectToMove, at: destinationIndexPath.row)
+        saveData()
+    }
+    
+    func saveData() {
+        if let encoded = try? JSONEncoder().encode(assignments) {
+            defaults.set(encoded, forKey: "data")
+        }
     }
     
 }
